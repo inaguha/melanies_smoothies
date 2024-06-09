22eb83b12aa05b7918db9dc2ffbc3760be439085
Ina Guha <65281591+inaguha@users.noreply.github.com>
2024-06-09 17:24:31 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,18 +1,15 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
-from snowflake.snowpark.context import get_active_session
-
+#from snowflake.snowpark.context import get_active_session
 # Write directly to the app
 st.title(":cup_with_straw: Customize your Smoothie :cup_with_straw:")
 st.write(
     """ Choose the fruits you want in your custom Smoothie!
     """
 )
-
 #st.title('This is a title')
 #st.title('_Streamlit_ is :blue[cool] :sunglasses:')
-
 name_on_order = st.text_input("Name on Smoothie: ")
 st.write("The Name on your Smoothie will be: ", name_on_order)
 
@@ -33,7 +30,7 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string+=fruit_chosen
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order) values ('""" + ingredients_string + """','""" + name_on_order + """')"""
-    time_to_insert = st.button('Submit Order', type="primary")
+    time_to_insert = st.button("Submit Order")
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
