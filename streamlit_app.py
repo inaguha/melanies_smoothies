5c46fd693fcaf2298caf2438008af4006dda73c6
Ina Guha <65281591+inaguha@users.noreply.github.com>
2024-06-09 17:12:52 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,8 +16,8 @@ st.write(
 name_on_order = st.text_input("Name on Smoothie: ")
 st.write("The Name on your Smoothie will be: ", name_on_order)
 
-cnx=st.connection("snowflake")
-session = cnx.session()
+st.connection("snowflake").session()
+
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True);
 
@@ -32,7 +32,6 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string+=fruit_chosen 
         my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order) values ('""" + ingredients_string + """','""" + name_on_order + """')"""
-        time_to_insert = st.button('Submit Order')
-        if time_to_insert:
+        if st.button('Submit Order'):
             session.sql(my_insert_stmt).collect()
             st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
