5bf40237518a47020ee1f0260a8741233b14931f
Ina Guha <65281591+inaguha@users.noreply.github.com>
2024-06-09 20:14:03 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
-import streamlit as st
 from snowflake.snowpark.functions import col
-#from snowflake.snowpark.context import get_active_session
+import streamlit as st
 cnx=st.connection("snowflake")
 session=cnx.session()
 st.title(":cup_with_straw: Customize your Smoothie :cup_with_straw:")
